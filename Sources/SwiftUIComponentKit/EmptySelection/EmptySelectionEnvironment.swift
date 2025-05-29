c9fb6852e2cc4c15f3d58bae72828a385d3e8914
Matheus Silveira Venturini <msventurini@icloud.com>
2025-05-29 05:22:32 -0300

    WIP: Adding Views to generic SwiftUI components module.

---
+++ Sources/SwiftUIComponentKit/EmptySelection/EmptySelectionEnvironment.swift
@@ -0,0 +1,16 @@
+//
+//  EmptySelectionContentsEnv.swift
+//  VentUI
+//
+//  Created by Matheus Silveira Venturini on 29/05/25.
+//
+
+import SwiftUI
+
+extension EnvironmentValues {
+    @Entry var emptySelectionEnvironment: EmptySelectionModel = .init(
+        labelText: "Nothing Here",
+        systemImage: "exclamationmark.square",
+        description: "Select some option."
+    )
+}
---
+++ Sources/SwiftUIComponentKit/EmptySelection/EmptySelectionModel.swift
@@ -0,0 +1,16 @@
+//
+//  EmptySelectionModel.swift
+//  VentUI
+//
+//  Created by Matheus Silveira Venturini on 29/05/25.
+//
+
+import Foundation
+
+struct EmptySelectionModel {
+    
+    let labelText: String
+    let systemImage: String
+    let description: String
+    
+}
