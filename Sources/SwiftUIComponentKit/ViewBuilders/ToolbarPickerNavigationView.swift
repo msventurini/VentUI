38fa8863ad7968b84ee353b7f9508f33dbcedda6
Matheus Silveira Venturini <msventurini@icloud.com>
2025-05-29 23:45:14 -0300

    WIP: Solving access controll issues.

--- Sources/SwiftUIComponentKit/ViewBuilders/ToolbarPickerNavigationView.swift
+++ Sources/SwiftUIComponentKit/ViewBuilders/ToolbarPickerNavigationView.swift
@@ -47,6 +47,47 @@ public struct ToolbarPickerNavigationView<Content, BottomToolbar, ItemType>: Vie
     }
 }
 
+public struct EmptyToolbarNavigationView<Content, BottomToolbar, ItemType>: View where Content: View, BottomToolbar: View, ItemType: Hashable & Identifiable {
+    
+    @State var selected: ItemType?
+    var collection: [ItemType]
+    let content: (ItemType) -> Content
+    let bottomToolbar: (Binding<ItemType?>, [ItemType]) -> BottomToolbar
+
+    @ViewBuilder var conditionalContent: some View {
+        if let selected {
+            content(selected)
+        } else {
+            EmptySelectionView()
+        }
+    }
+    
+    public init(
+        collection: [ItemType],
+        @ViewBuilder content: @escaping (ItemType) -> Content,
+        bottomToolbar: @escaping (Binding<ItemType?>, [ItemType]) -> BottomToolbar
+    ) {
+        self.collection = collection
+        self.selected = collection.first
+        self.content = content
+        self.bottomToolbar = bottomToolbar
+    }
+    
+    public var body: some View {
+        
+        NavigationStack {
+            VStack {
+                conditionalContent
+            }
+            .toolbar {
+                bottomToolbar($selected, collection)
+            }
+        }
+        
+    }
+}
+
+
 // fazer uma forma de usar com o comportamento anterior depois
 //public struct ToolbarPickerNavigationView<Content, BottomToolbar, ItemType>: View where Content: View, BottomToolbar: ToolbarContent, ItemType: Hashable & Identifiable {
 //    
